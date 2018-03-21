0efed1a2daa7abde8a765af1eacc123d561662b4
mforjan <mforjan@andrew.cmu.edu>
2018-03-20 20:08:21 -0700

    almost fully functional font size/font color menus

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 // import Login from './components/login';
-import Register from './components/register'
+// import Register from './components/register'
+import TextEditor from './components/Editor';
 
 export default class App extends React.Component {
   render() {
     return (<div>
-      <Register />
+      <TextEditor />
     </div>);
   }
 }
