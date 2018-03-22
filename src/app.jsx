ee6b203c2b43fe698756a928c6a90d77097a670f
darocca <taav.kujira@gmail.com>
2018-03-22 15:07:43 -0700

    add new document, get all documents for logged in user

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Login from './components/login';
 import Register from './components/register';
-import TextEditor from './components/editor';
+import Editor from './components/editor';
 import Home from './components/home'
 import { HashRouter, Switch, Route } from 'react-router-dom';
 
@@ -15,6 +15,7 @@ export default class App extends React.Component {
                 <Route exact path="/login" component={Login}/>
                 <Route exact path="/register" component={Register}/>
                 <Route exact path="/home" component={Home}/>
+                <Route exact path="/editor" component={Editor}/>
             </Switch>
         </HashRouter>
       </div>
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -15,13 +15,13 @@ class Home extends React.Component {
     super(props);
     this.state = {
         open: false,
-        title: 'Untitled',
+        titleInput: 'Untitled',
     }
     this.handleOpen = this.handleOpen.bind(this)
     this.handleClose = this.handleClose.bind(this)
     this.handleLogOut = this.handleLogOut.bind(this)
     this.handleTitleChange = this.handleTitleChange.bind(this)
-    // this.handleNewDoc = this.handleNewDoc.bind(this)
+    this.handleNewDoc = this.handleNewDoc.bind(this)
   }
 
   handleOpen() {
@@ -38,8 +38,7 @@ class Home extends React.Component {
           headers: {
               'Accept': 'application/json',
               'Content-Type': 'application/json',
-              'Authorization': `Bearer ${jwt.sign({
-              }, process.env.JWT_SECRET)}`
+              'Authorization': global.token
           }
       })
       .then(res => res.json())
@@ -57,29 +56,52 @@ class Home extends React.Component {
     this.setState({
       titleInput: event.target.value
     })
-    alert("Hi David!")
   }
 
-  // handleNewDoc() {
-  //     fetch('http://localhost:3000/home', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Accept': 'application/json',
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify({
-  //           title: this.state.titleInput,
-  //       })
-  //     })
-  //     .then(res => res.json())
-  //     .then((result) => {
-  //         this.props.history.push('/editor');
-  //         alert("Success!")
-  //     })
-  //     .catch((error) => {
-  //         console.log("Error: ", error)
-  //     })
-  // }
+  handleNewDoc() {
+      fetch('http://localhost:3000/doc/new', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'Authorization': global.token
+        },
+        body: JSON.stringify({
+            title: this.state.titleInput,
+        })
+      })
+      .then(res => res.json())
+      .then((result) => {
+          console.log(result);
+          // alert("Success!");
+          this.handleClose();
+          this.props.history.push('/editor');
+      })
+      .catch((error) => {
+          console.log("Error: ", error)
+      })
+  }
+
+  componentDidMount(){
+    console.log('mount')
+    fetch('http://localhost:3000/home', {
+          method: 'GET',
+          headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json',
+              'Authorization': global.token
+          }
+      })
+      .then(res => res.json())
+      .then((result) => {
+          if (result.success) {
+            console.log('/home', result);
+          }
+      })
+      .catch((error) => {
+          console.log("Error: ", error)
+      })
+  }
 
   render() {
       const actions = [
@@ -90,7 +112,8 @@ class Home extends React.Component {
           <RaisedButton
             label="Submit"
             primary={true}
-            onClick={this.handleClose}
+            onClick={this.handleNewDoc}
+            type="submit"
           />,
         ];
       const center = {
@@ -103,23 +126,20 @@ class Home extends React.Component {
                   <AppBar title={`Welcome, ${global.displayName || '?'}`}/>
                       <div>
                         <RaisedButton label="Create a new Document" primary={true} onClick={this.handleOpen} />
-                        {/* <form onSubmit={(e) => this.handleNewDoc(e)}> */}
                             <Dialog
                               title="Create a new Document"
                               actions={actions}
                               modal={false}
                               open={this.state.open}
-                              onRequestClose={this.handleClose}
-                            >
+                              onRequestClose={this.handleClose}>
                               <TextField
                                    hintText="Give your Document a title"
                                    floatingLabelText="Document"
-                                   // onChange={(e) => this.handleTitleChange(e)}
+                                   onChange={(e) => this.handleTitleChange(e)}
                                    // value={this.state.titleInput}
                                    // errorText={this.state.emailError}
                                />
                             </Dialog>
-                        {/* </form> */}
                         <RaisedButton onClick={this.handleLogOut} label="Log Out" />
                       </div>
                   </div>
